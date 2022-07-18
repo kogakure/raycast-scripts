b43c0874fefa58b08e668b2151848a46adb87cdc
Stefan Imhoff <stefan@imhoff.name>
2022-07-18 18:47:47 +0200

    feat: add script to automatically copy IMDb CSV backup files to Dropbox

---
+++ scripts/imdb-backup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title IMDb Backup
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🛟
+# @raycast.packageName Backup
+
+# Documentation:
+# @raycast.description Copies my IMDb backup files to Dropbox
+# @raycast.author Stefan Imhoff
+# @raycast.authorURL https://www.stefanimhoff.de
+
+TARGET="$(date +"%Y-%m-%d")"
+
+cd ~/Downloads
+mkdir "$TARGET"
+mv *.csv "$TARGET"
+mv "$TARGET" ~/Dropbox/Backups/IMDb/
